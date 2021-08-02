59531eb5b5dafb6186c7772272a24a4faa658b0f
artem_gg <gmadrich@gmail.com>
2021-08-03 00:46:34 +0300

    валидация

--- src/components/AddContractorModal.vue
+++ src/components/AddContractorModal.vue
@@ -1,56 +1,97 @@
 <template>
-    <b-modal
-      id="add-modal"
-      centered
-      title="Добавление"
-      @ok="AddContractor"
-      ref="modal"
-    >
-      <b-form ref="form">
-        <b-input-group class="mb-2" prepend="Название">
-          <b-form-input
-            v-model="Contractor.Name"
-            placeholder="Введите название"
-            required
-          ></b-form-input>
-        </b-input-group>
-        <b-input-group
-          class="mb-2"
-          prepend="Тип"
-          invalid-feedback="Заполните поле"
+  <b-modal
+    id="add-modal"
+    centered
+    title="Добавление"
+    @ok="AddContractor"
+    hide-footer
+  >
+    <validation-observer ref="observer" v-slot="{ handleSubmit }">
+      <b-form @submit.stop.prevent="handleSubmit(onSubmit(bvModal))">
+        <validation-provider
+          name="Название"
+          :rules="{ required: true, min: 3 }"
+          v-slot="validationContext"
         >
-          <b-form-select
+          <b-input-group class="mb-2" prepend="Название">
+            <b-form-input
+              v-model="Contractor.Name"
+              placeholder="Введите название"
+              :state="getValidationState(validationContext)"
+            ></b-form-input>
+          </b-input-group>
+          <b-form-invalid-feedback id="input-1-live-feedback">{{
+            validationContext.errors[0]
+          }}</b-form-invalid-feedback>
+        </validation-provider>
+
+        <validation-provider
+          name="ContractorType"
+          :rules="{ required: true }"
+          v-slot="validationContext"
+        >
+          <b-input-group
             class="mb-2"
-            v-model="Contractor.Type"
-            :options="TypeOptions"
-            required
-          ></b-form-select>
-        </b-input-group>
-        <b-input-group
-          class="mb-2"
-          prepend="ИНН"
-          invalid-feedback="Заполните поле"
+            prepend="Тип"
+            invalid-feedback="Заполните поле"
+          >
+            <b-form-select
+              class="mb-2"
+              v-model="Contractor.Type"
+              :options="TypeOptions"
+              :state="getValidationState(validationContext)"
+            ></b-form-select>
+            <b-form-invalid-feedback id="input-2-live-feedback">{{
+              validationContext.errors[0]
+            }}</b-form-invalid-feedback>
+          </b-input-group>
+        </validation-provider>
+
+        <validation-provider
+          name="ИНН"
+          :rules="{ reauired: true }"
+          v-slot="validationContext"
         >
-          <b-form-input
-            v-model="Contractor.INN"
-            placeholder="Введите инн"
-            required
-          ></b-form-input>
-        </b-input-group>
-        <b-input-group
-          class="mb-2"
-          prepend="КПП"
-          v-if="Contractor.Type == 'Legal'"
-          invalid-feedback="Заполните поле"
+          <b-input-group
+            class="mb-2"
+            prepend="ИНН"
+            invalid-feedback="Заполните поле"
+          >
+            <b-form-input
+              v-model="Contractor.INN"
+              placeholder="Введите инн"
+              :state="getValidationState(validationContext)"
+            ></b-form-input>
+            <b-form-invalid-feedback>{{
+              validationContext.errors[0]
+            }}</b-form-invalid-feedback>
+          </b-input-group>
+        </validation-provider>
+        <validation-provider
+          name="КПП"
+          :rules="{ required: true }"
+          v-slot="validationContext"
         >
-          <b-form-input
-            v-model="Contractor.KPP"
-            placeholder="Введите кпп"
-            required
-          ></b-form-input>
-        </b-input-group>
+          <b-input-group
+            class="mb-2"
+            prepend="КПП"
+            v-if="Contractor.Type == 'Legal'"
+            invalid-feedback="Заполните поле"
+          >
+            <b-form-input
+              v-model="Contractor.KPP"
+              placeholder="Введите кпп"
+              :state="getValidationState(validationContext)"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-2-live-feedback">{{
+              validationContext.errors[0]
+            }}</b-form-invalid-feedback>
+          </b-input-group>
+        </validation-provider>
+        <b-button id="submit-btn" type="submit" variant="primary">Добавить</b-button>
       </b-form>
-    </b-modal>
+    </validation-observer>
+  </b-modal>
 </template>
 
 <script>
@@ -81,6 +122,12 @@ export default {
         body: JSON.stringify(this.Contractor),
       }).then((response) => response.json());
     },
+    getValidationState({ dirty, validated, valid = null }) {
+      return dirty || validated ? valid : null;
+    },
+    onSubmit(){
+      this.$refs['add-modal'].hide();
+    }
   },
 };
 </script>
--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,24 @@
-import Vue from 'vue'
+import Vue from "vue";
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize,
+} from "vee-validate";
+import ru from "vee-validate/dist/locale/ru.json";
+import * as rules from "vee-validate/dist/rules";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+Object.keys(rules).forEach((rule) => {
+  extend(rule, rules[rule]);
+});
 
-Vue.use(BootstrapVue)
+localize("ru", ru);
+
+// Install VeeValidate components globally
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
+
+Vue.use(BootstrapVue);
